30cd4aed1b17aba4e64c0d06fae9a735954478ea
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-05-14 16:18:39 +0530

    [Varsha Karthikeyan]: Added core user module structure

---
+++ src/main/java/com/digitalgoldwallet/digital_gold_wallet/entity/User.java
@@ -0,0 +1,4 @@
+package com.digitalgoldwallet.digital_gold_wallet.entity;
+
+public class User {
+}
---
+++ src/main/java/com/digitalgoldwallet/digital_gold_wallet/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.digitalgoldwallet.digital_gold_wallet.repository;
+
+public class UserRepository {
+}
